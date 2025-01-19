91effb92ae4a44bed2fd7406b4dc1b851954bfba
robbeattyprogramming <robbeattyprogramming@gmail.com>
2025-01-18 19:30:31 -0500

    created program intro and the math operations user can select from

--- small_c_sharp_projects/math_practice/Program.cs
+++ small_c_sharp_projects/math_practice/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Dictionary<int, string> operationSelectionList = new Dictionary<int, string>();
+operationSelectionList.Add(1, "Addition");
+operationSelectionList.Add(2, "Subtraction");
+operationSelectionList.Add(3, "Multiplication");
+operationSelectionList.Add(4, "Division");
+
+Console.WriteLine("Which math operation would you like to practice?\n");
+for(int i = 1; i < 5; i++)
+{
+    Console.WriteLine($"{i}: {operationSelectionList[i]}");
+}
+Console.WriteLine("\nPlease select by inputing a number from 1-4");
+int userSelection = Int32.Parse(Console.ReadLine());
