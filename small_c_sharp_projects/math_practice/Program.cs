cd2cc3f02fd7bdb525986de081d0b4e0650250f4
robbeattyprogramming <robbeattyprogramming@gmail.com>
2025-01-18 20:33:18 -0500

    added while loop to the program and option for user termination. Built addition option. Need to learn csharp functions

--- small_c_sharp_projects/math_practice/Program.cs
+++ small_c_sharp_projects/math_practice/Program.cs
@@ -6,10 +6,83 @@ operationSelectionList.Add(2, "Subtraction");
 operationSelectionList.Add(3, "Multiplication");
 operationSelectionList.Add(4, "Division");
 
-Console.WriteLine("Which math operation would you like to practice?\n");
-for(int i = 1; i < 5; i++)
+while (true)
 {
-    Console.WriteLine($"{i}: {operationSelectionList[i]}");
+    Console.WriteLine("Which math operation would you like to practice? Enter 5 to end program.\n");
+    
+    for (int i = 1; i < 5; i++)
+    {
+        Console.WriteLine($"{i}: {operationSelectionList[i]}");
+    }
+    Console.WriteLine("\nPlease select by inputing a number from 1-4:\n");
+    int userSelection = Int32.Parse(Console.ReadLine());
+
+    if (userSelection == 1)
+    {
+        while (true)
+        {
+            Console.Clear(); 
+
+            Random rnd = new Random();
+            int firstValue = rnd.Next(1,100);
+            int secondValue = rnd.Next(1, 100);
+            int operationAnswer = firstValue + secondValue;
+
+            // turn into a function later and apply to each section
+            Console.WriteLine($"Addition: {firstValue} + {secondValue}");
+            int userAnswer = Int32.Parse(Console.ReadLine());
+
+            if (userAnswer == operationAnswer)
+            {
+                Console.WriteLine("CORRECT! Input 1 for another multiplication problem. Input 2 to select a new operation problem");
+                int userContinueOption = Int32.Parse(Console.ReadLine());
+                if (userContinueOption == 1)
+                {
+                    continue;
+                }
+                else if (userContinueOption == 2)
+                {
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"nope! Answer is: {operationAnswer}. Input 1 for another multiplication problem. Input 2 to select a new operation problem");
+                int userContinueOption = Int32.Parse(Console.ReadLine());
+                if (userContinueOption == 1)
+                {
+                    continue;
+                }
+                else if (userContinueOption == 2)
+                {
+                    break;
+                }
+            }
+
+
+        }
+    }
+    else if (userSelection == 2)
+    {
+        Console.WriteLine("2");
+    }
+    else if (userSelection == 3)
+    {
+        Console.WriteLine("3");
+    }
+    else if (userSelection == 4)
+    {
+        Console.WriteLine("4");
+    }
+    else if (userSelection == 5)
+    {
+        Console.WriteLine("5");
+        break;
+    }
+    else 
+    {
+        Console.WriteLine("Please select an option by entering a number between 1 and 4.\n");
+        Console.Clear();
+    }
 }
-Console.WriteLine("\nPlease select by inputing a number from 1-4");
-int userSelection = Int32.Parse(Console.ReadLine());
+
